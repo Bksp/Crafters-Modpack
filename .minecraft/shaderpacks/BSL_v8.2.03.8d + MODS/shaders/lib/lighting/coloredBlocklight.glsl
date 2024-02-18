02e0b151d04dd9894bd7aa664530b58418cd0bd7
Alessy <fabian.cares.d@gmail.com>
2024-02-18 16:25:23 -0300

    Shader

--- .minecraft/shaderpacks/BSL_v8.2.03.8d + MODS/shaders/lib/lighting/coloredBlocklight.glsl
+++
@@ -1,9 +0,0 @@
-vec3 ApplyMultiColoredBlocklight(vec3 blocklightCol, vec2 screenPos) {
-	vec3 coloredLight = texture2D(colortex9, screenPos.xy).rgb;
-		
-	vec3 coloredLightNormalized = normalize(coloredLight + 0.00001);
-	coloredLightNormalized *= GetLuminance(blocklightCol) / GetLuminance(coloredLightNormalized);
-	float coloredLightMix = min((coloredLight.r + coloredLight.g + coloredLight.b) * 256.0, 1.0);
-
-	return mix(blocklightCol, coloredLightNormalized, coloredLightMix);
-}
