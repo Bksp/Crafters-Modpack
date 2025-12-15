341119e6f3895bd36314c2e45c4458ea7474a3f3
Alessy <fabian.cares.d@gmail.com>
2025-12-15 19:13:56 -0300

    1.0.3 Beta

---
+++ .minecraft/Actualizar.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Buscando actualizaciones en el GitHub de Logan...
+java -jar packwiz-installer-bootstrap.jar https://raw.githubusercontent.com/Bksp/Crafters-Modpack/main/.minecraft/pack.toml
+echo.
+echo ¡Listo! Tu juego esta actualizado.
+pause
