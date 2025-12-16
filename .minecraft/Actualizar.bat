5382754be58076cc6869fffa29ba85670e2999b5
Alessy <fabian.cares.d@gmail.com>
2025-12-16 19:01:44 -0300

    Auto-deploy version 1.0.1

--- .minecraft/Actualizar.bat
+++ .minecraft/Actualizar.bat
@@ -1,6 +1,28 @@
 @echo off
-echo Buscando actualizaciones en el GitHub de Logan...
-java -jar packwiz-installer-bootstrap.jar https://raw.githubusercontent.com/Bksp/Crafters-Modpack/main/.minecraft/pack.toml
+title ACTUALIZADOR CRAFTERS MODPACK
+color 0B
+cls
+echo ========================================================
+echo   CONECTANDO CON GITHUB PARA ACTUALIZAR MODS...
+echo ========================================================
 echo.
-echo ¡Listo! Tu juego esta actualizado.
+
+:: Comprobamos si existe el archivo necesario
+if not exist .\packwiz-installer-bootstrap.jar (
+    color 0C
+    echo [ERROR] No encuentro el archivo 'packwiz-installer-bootstrap.jar'.
+    echo Asegurate de que este archivo .bat este en la misma carpeta que el .jar
+    pause
+    exit
+)
+
+:: Ejecuta la actualizacion usando la ruta relativa .\
+:: Esta es la URL RAW de tu pack.toml
+java -jar .\packwiz-installer-bootstrap.jar https://raw.githubusercontent.com/Bksp/Crafters-Modpack/main/.minecraft/pack.toml
+
+echo.
+echo ========================================================
+echo   TODO LISTO. YA PUEDES ABRIR EL LAUNCHER.
+echo ========================================================
+echo.
 pause
